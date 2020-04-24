720719bc3389b4c7bfe3254bb377b0fb80768af1
Hung Nguyen Quoc <n.q.hung@azoom.jp>
2020-04-24 14:54:06 +0700

    :+1: Implement analysis slash command

---
+++ app/helpers/slash-command-parser.js
@@ -0,0 +1,48 @@
+import toLower from 'lodash/fp/toLower'
+
+export default (commandText, seperator = '=') => {
+  const commandArgs = commandText.match(/\S+/g) || false
+  if (!commandArgs) return {}
+  return commandArgs.reduce((formatedArgs, arg) => {
+    const [generalKey, value] = arg.split(seperator)
+    const key = {
+      'username': 'username',
+      'u': 'username',
+      'password': 'password',
+      'p': 'password',
+      'email': 'email',
+      'e': 'email',
+      'permissionId': 'permissionId',
+      'pid': 'permissionId',
+      'userId': 'userId',
+      'uid': 'userId',
+      'projectId': 'projectId',
+      'pjid': 'projectId',
+      'projectName': 'projectName',
+      'pjn': 'projectName',
+      'managerId': 'managerId',
+      'mid': 'managerId',
+      'memberIds': 'memberIds',
+      'mids': 'memberIds',
+      'position': 'position',
+      'pos': 'position',
+      'startTime': 'startTime',
+      'st': 'startTime',
+      'endTime': 'endTime',
+      'et': 'endTime',
+      'date': 'date',
+      'd': 'date',
+      'reasion': 'reasion',
+      'rs': 'reasion',
+      'status':'status',
+      's':'status',
+      'timesheetAppId': 'timesheetAppId',
+      'atid': 'timesheetAppId',
+      'leaveType': 'leaveType',
+      'lt': 'leaveType',
+      'leavesAppId': 'leavesAppId',
+      'alid': 'leavesAppId'
+    }[generalKey]
+    return value ? { ...formatedArgs, [key]: value } : formatedArgs
+  }, {})
+}
---
+++ app/routes/slash-command/post.js
@@ -0,0 +1,137 @@
+import commandParser from '@helpers/slash-command-parser'
+import { execute } from '@root/util.js'
+import getUserDetail from '@routes/users/_userId/get.js'
+import getUserBySlackId from '@routes/users/_slackId/get.js'
+import checkIn from '@routes/timesheets/checkin/post.js'
+import checkOut from '@routes/timesheets/checkout/post.js'
+
+exports.post = async (req, res) => {
+  const request = req.body
+  const { user_id } = req.body
+  const commandText = req.body.text
+
+  // TODO Remove comment below when pubsub and upcloud deployment will be implemented
+  // if (!verifySlackRequest(request))
+  //   await slackPostMessage(request.body.user_id, 'We are sorry but we are not able to authenticate you.')
+
+  const user = await execute(getUserBySlackId, { params: { slackId: user_id } } )
+  if (!user)
+    return await slackPostMessage(userId, 
+      `> Sorry, You did not register account before
+      > Please register HRM account first`)
+  const params = commandParser(commandText)
+  const {resource, action} = getSlashCommandAction(commandText)
+  const slashCommands = {
+    ':': () => {
+      return `
+        -------------------------------------------------------------------------
+         R \`/hrm\` → Show help
+         R \`/hrm users:list\` → Get all user
+         R \`/hrm users:profile\` → Get an user detail
+         R \`/hrm checkin\` → Check in
+         R \`/hrm checkout\` → Check out
+         -------------------------------------------------------------------------
+      `
+    },
+    'users:profile': async ({ params }) => {
+      return execute(getUserDetail, { params: { userId: params.userId } } )
+    },
+
+    'users:list': async ({ user }) => {},
+    'users:create': async ({ user, params }) => {},
+    'users:permission': async ({ user, params }) => {},
+    'users:deactive': async ({ user, params }) => {},
+    'users:update': async ({ user, params }) => {},
+    'permission:list': async ({ user, params }) => {},
+    'projects:list': async ({ user, params }) => {},
+    'projects:create': async ({ user, params }) => {},
+    'projects:add-member': async ({ user, params }) => {},
+    'projects:update-member': async ({ user, params }) => {},
+    'projects:remove-member': async ({ user, params }) => {},
+    'checkin:': async ({ user }) => {
+      return execute(checkIn, { body: { userId: user.id } } )
+    },
+    'checkout:': async ({ user }) => {
+      return execute(checkOut, { body: { userId: user.id } } )
+    },
+    'timesheets': async ({ user, params }) => {},
+    'applications-timesheets:create': async ({ user, params }) => {},
+    'applications-timesheets:list': async ({ user, params }) => {},
+    'applications-timesheets:approval': async ({ user, params }) => {},
+    'applications-timesheets:remove': async ({ user, params }) => {},
+    'applications-timesheets:all': async ({ user, params }) => {},
+    'application-leaves:create': async ({ user, params }) => {},
+    'application-leaves:list': async ({ user, params }) => {},
+    'application-leaves:approval': async ({ user, params }) => {},
+    'application-leaves:delete': async ({ user, params }) => {},
+  }
+
+  // TODO Send response to Slack via postMessage or response_url in requestBody
+  const result = await slashCommands[`${resource}:${action}`]({user, action, params})
+
+  return res.status(200).send(result)
+}
+
+const getSlashCommandAction = commandText => {
+  if (!commandText) return {
+    resource: "",
+    action: ""
+  }
+  const [resourceArg = "", actionAndParam = ""] = commandText.split(':', 2)
+  const [actionArg] = actionAndParam.split(' ', 1)
+  const resource = {
+    'u': 'users',
+    'user': 'users',
+    'users': 'users',
+    'permission': 'permission',
+    'permissions': 'permission',
+    'per': 'permission',
+    'projects': 'projects',
+    'project': 'projects',
+    'p': 'projects',
+    'checkin': 'checkin',
+    'in': 'checkin',
+    'checkout': 'checkout',
+    'out': 'checkout',
+    'timesheet': 'timesheets',
+    'timesheets': 'timesheets',
+    'ts': 'timesheets',
+    'applications-timesheet': 'applications-timesheets',
+    'applications-timesheets': 'applications-timesheets',
+    'ats': 'applications-timesheets',
+    'application-leave': 'application-leaves',
+    'application-leaves': 'application-leaves',
+    'al': 'application-leaves'
+  }[resourceArg] || ""
+  const action = {
+    'l': 'list',
+    'list': 'list',
+    'c': 'create',
+    'create': 'create',
+    'per': 'permission',
+    'permission': 'permission',
+    'd': 'deactive',
+    'deactive': 'deactive',
+    'u': 'update',
+    'update': 'update',
+    'p': 'profile',
+    'profile': 'profile',
+    'am': 'add-member',
+    'add-member': 'add-member',
+    'um': 'update-member',
+    'update-member': 'update-member',
+    'rm': 'remove-member',
+    'remove-member': 'remove-member',
+    'ap': 'approval',
+    'approval': 'approval',
+    'r': 'remove',
+    'remove': 'remove',
+    'all': 'all',
+    'del': 'delete'
+  }[actionArg] || ""
+
+  return {
+    resource,
+    action
+  }
+}
